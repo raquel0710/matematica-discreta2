50052db0fab4340bab699002ed34a0380b139e19
raquel0710 <50027549+raquel0710@users.noreply.github.com>
2019-07-18 01:45:15 -0300

    Create QUESTAO1.py

---
+++ QUESTAO1.py
@@ -0,0 +1,13 @@
+def recursividade(n,k):
+    if k == 0 or k==n:
+        return 1
+    else:
+        return ((n - k + 1) * recursividade(n, k - 1)) // k
+
+
+
+n = int(input("Insira o valor de n: "))
+p = int(input("Insira o valor de p: "))
+k = p
+print(recursividade(n, k))
+

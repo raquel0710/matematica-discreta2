a3b95e460200bd16888ce298e775032dd6fb6e23
Débora Silva <50027549+raquel0710@users.noreply.github.com>
2019-10-08 11:37:30 -0300

    Create teste.papel.py

---
+++ teste.papel.py
@@ -0,0 +1,8 @@
+N = int(input())
+lista = []
+lista = [() for m in range(N)]
+for i in range(N):
+    h = int(input())
+    lista.append(h)
+print(h) #não estou conseguindo adicionar h a cada tupla
+print(lista)

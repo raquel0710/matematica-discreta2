7978c307d75287ba5620afb15b80f1a1c8efd21a
Débora Silva <50027549+raquel0710@users.noreply.github.com>
2019-10-17 20:22:22 -0300

    Update dividindoimperio_teste.py

--- dividindoimperio_teste.py
+++ dividindoimperio_teste.py
@@ -1,27 +1,22 @@
+def dividindo_imperio( i, pai ):
+    global resposta
+    peso_arestas = 1
+    for j in grafo[i]:
+        if ( j != pai ):
+            peso_arestas += dividindo_imperio( j, i )
+    diferenca = abs(N-2*peso_arestas)
+    if ( diferenca < resposta ):
+        resposta = diferenca
+    return peso_arestas
 
-def dfs( i, pai ):
-    global res
-    peso = 1
-    for p in g[i]:
-        if ( p != pai ):
-            peso += dfs( p, i )
-    dif = abs(N-2*peso)
-    if ( dif < res ):
-        res = dif
-    return peso
-
-[N] = [int(c) for c in input().split()]
-
-g = [[] for _ in range(N+1)]
+[N] = [int(p) for p in input().split()]
+grafo = [[] for _ in range(N+1)]
 
 for i in range(1,N):
-    [A, B] = [int(c) for c in input().split()]
-    g[A].append( B )
-    g[B].append( A )
+    [A, B] = [int(p) for p in input().split()]
+    grafo[A].append( B )
+    grafo[B].append( A )
+resposta = N
 
-res = N
-
-
-dfs( 1, -1 )
-
-print(res)
+dividindo_imperio( 1, -1 )
+print(resposta)
